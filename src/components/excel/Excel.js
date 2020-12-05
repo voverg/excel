212bbc08346bca141cd381e2b97181324bc32ad5
voverg <voverg81@mail.ru>
2020-12-05 15:30:36 +0300

    Create add events functionality

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -10,11 +10,13 @@ export class Excel {
     getRoot() {
         const $root = $.create('div', 'excel');
 
-        this.components.forEach(Component => {
+        this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className);
             const component = new Component($el);
             $el.html(component.toHtml());
             $root.append($el);
+
+            return component;
         });
 
         return $root;
@@ -22,5 +24,7 @@ export class Excel {
 
     render() {
         this.$el.append(this.getRoot());
+
+        this.components.forEach(component => component.init());
     }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -2,7 +2,16 @@ import {DomListener} from '@core/DomListener.js';
 
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners);
+        this.name = options.name || '';
+    }
+
     toHtml() {
         return '';
+    }
+
+    init() {
+        this.initDOMListeners();
     }
 }
