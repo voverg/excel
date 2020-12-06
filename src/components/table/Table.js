b5bc7658122aff9b026e9ec43f5a1afc29502df4
voverg <voverg81@mail.ru>
2020-12-06 19:23:57 +0300

    Start create resize functionality

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,6 +5,13 @@ import {createTable} from '@/components/table/table.template.js';
 export class Table extends ExcelComponent {
     static className = 'excel__table';
 
+    constructor($root) {
+        super($root, {
+            name: Table,
+            listeners: []
+        })
+    }
+
     toHtml() {
         return createTable(20);
     }
