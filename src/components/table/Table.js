4da2dbfd60c0f7f090c3010f323298831685dfea
voverg <voverg81@mail.ru>
2020-12-09 13:34:02 +0300

    Create resize columns and rows functional

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent.js';
 import {createTable} from '@/components/table/table.template.js';
+import {resizeHandler} from '@/components/table/table.resize.js';
+import {shouldResize} from '@/components/table/table.functions.js';
 import {$} from '@core/dom.js';
 
 
@@ -18,34 +20,8 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event) {
-        if (event.target.dataset.resize) {
-            const $resizer = $(event.target);
-            const $parent = $resizer.closest('[data-type="resizable"]');
-            const coords = $parent.getCoords();
-            const type = $resizer.data.resize;
-
-            console.log(type);
-
-            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
-
-            document.onmousemove = e => {
-                console.log('mousemove');
-                if (type === 'col') {
-                const delta = e.pageX - coords.right;
-                const value = coords.width + delta;
-                $parent.css({width: value + 'px'});
-                cells.forEach(el => el.style.width = value + 'px');
-                } else {
-                    const delta = e.pageY - coords.bottom;
-                    const value = coords.height + delta;
-                    $parent.css({height: value + 'px'});
-                }
-
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null;
-            }
+        if (shouldResize(event)) {
+            resizeHandler(this.$root, event);
         }
     }
 }
